e0e674668c308d2ec928c009d1338e1cc82fc6f0
CuttleFish <b9804320975@gmail.com>
2024-01-19 21:09:05 +0800

    logo 模組化 js&css資料夾分類成功

---
+++ src/main/resources/static/js/com/nav/showHotList.js
@@ -0,0 +1,38 @@
+function showHotList() {
+    $("#hot-list").empty();
+    $.ajax({
+        url: "/products/hot_list",
+        type: "GET",
+        dataType: "JSON",
+        success: function (json) {
+            let list = json.data;
+            console.log("count=" + list.length);
+            for (let i = 0; i < list.length; i++) {
+                console.log(list[i].title);
+                // let hot =
+                //     '<div class="col-md-12">'
+                //     + '<div class="col-md-7 text-row-2"><a href="product.html?id=#{id}">#{title}</a></div>'
+                //     + '<div class="col-md-2">$#{price}</div>'
+                //     + '<div class="col-md-3"><img src="..#{image}collect.png" class="img-responsive" /></div>'
+                //     + '</div>';
+                let hot =
+                    '<div class="">'
+                    + '<div class="list-sub">'
+                    + '<a href="product.html?id=#{id}" class="product-title-a">'
+                    + '<img src="..#{image}collect.png" class="" />'
+                    + '<p>#{title}</p>'
+                    + '</a>'
+                    + '<div class="">$#{price}</div>'
+                    + '</div>'
+                    + '</div>';
+
+                hot = hot.replace(/#{id}/g, list[i].id);
+                hot = hot.replace(/#{title}/g, list[i].title);
+                hot = hot.replace(/#{price}/g, list[i].price);
+                hot = hot.replace(/#{image}/g, list[i].image);
+
+                $("#hot-list").append(hot);
+            }
+        }
+    });
+}
