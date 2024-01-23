de40c6ffe24e5ccfcb90014d592292e79045b6bd
CuttleFish <b9804320975@gmail.com>
2024-01-23 09:22:44 +0800

    Test backstage ok

--- src/main/java/com/cy/store/controller/BackStageController.java
+++ src/main/java/com/cy/store/controller/BackStageController.java
@@ -1,29 +1,16 @@
 package com.cy.store.controller;
 
-import com.cy.store.entity.User;
-import com.cy.store.service.BackStageUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.List;
-
-
-@RequestMapping("backStage")
 @Controller
+@RequestMapping("backStage")
 public class BackStageController {
 
     @GetMapping("/index")
-    public void getAllUserData(HttpServletResponse response) throws IOException {
+    public String getAllUserData() {
 
-        response.setContentType("text/html");
-
-        String htmlContent = "<html><body><h1>Hello, World!</h1></body></html>";
-        response.getWriter().write(htmlContent);
+        return "forward:/web/BackStage/index.html";
     }
 }
