9fdde7349e54152bb8e0ec6070b782e496ed2dde
CuttleFish <b9804320975@gmail.com>
2024-01-26 17:07:33 +0800

    後台首頁 會員管理 前端 init

--- src/main/resources/static/js/backStage/getProductData.js
+++ src/main/resources/static/js/backStage/getProductData.js
@@ -48,8 +48,8 @@ function getProductData() {
                 tr.append('<td>' + product.modifiedTime + '</td>');
 
                 tr.append('<td>' +
-                    '<button class="btn btn-success btn-sm">修改</button>' +
-                    '<button class="btn btn-warning btn-sm">刪除</button>' +
+                    '<button class="btn btn-success btn-sm producUpData" onclick="productUpdataEvent(this)">修改</button>' +
+                    // '<button class="btn btn-warning btn-sm">刪除</button>' +
                     '</td>');
                 tbody.append(tr);
             });
@@ -63,3 +63,73 @@ function getProductData() {
     });
 }
 
+
+
+function productUpdataEvent(clickedButton) {
+    const producUpData_btnList = document.querySelectorAll(".producUpData");
+    const index = Array.prototype.indexOf.call(producUpData_btnList, clickedButton) + 1;
+    alert("您點擊的是第 " + (index) + " 個按鈕");
+
+
+    $.ajax({
+
+        url: '/BackStage/product/' + index,
+        type: 'get',
+        dataType: 'json',
+        success: function (data) {
+            let userList = data;
+            let memberContainer = $('#member');
+            memberContainer.empty();
+
+            let table = $('<table></table>').addClass('table');
+
+            let thead = $('<thead></thead>');
+            thead.append('<tr>' +
+                '<th class="member-list-th">商品id</th>' +
+                '<th class="member-list-th">分類id</th>' +
+                '<th class="member-list-th">商品系列</th>' +
+                '<th class="member-list-th">商品標題</th>' +
+                '<th class="member-list-th">商品賣點</th>' +
+                '<th class="member-list-th">商品單價</th>' +
+                '<th class="member-list-th">庫存數</th>' +
+                '<th class="member-list-th">圖片</th>' +
+                '<th class="member-list-th">商品狀態</th>' +
+                '<th class="member-list-th">優先級</th>' +
+                '<th class="member-list-th">創建時間</th>' +
+                '<th class="member-list-th">最後修改時間</th>' +
+                '</tr>');
+            table.append(thead);
+
+            let tbody = $('<tbody></tbody>');
+            // $.each(userList, function (index, user) {
+            let tr = $('<tr></tr>');
+            // tr.append('<td><input class="user-input" name="uid" value="' + userList.uid + '"></td>');
+            // tr.append('<td><input class="user-input" name="username" value="' + userList.username + '"></td>');
+            // tr.append('<td><input value="' + userList.password + '"></td>');
+            // tr.append('<td><input value="' + userList.salt + '"></td>');
+            tr.append('<td name="uid" >' + userList.uid + '</td>');
+            tr.append('<td name="username" >' + userList.username + '</td>');
+            tr.append('<td><input class="user-input" name="phone" value="' + userList.phone + '"></td>');
+            tr.append('<td><input class="user-input" name="email" value="' + userList.email + '"></td>');
+            // tr.append('<td><input value="' + (userList.gender == 0 ? '女生' : '男生') + '"></td>');
+            tr.append('<td><input name="gender" type="radio" name="gender" value="0" ' + (userList.gender == 0 ? 'checked' : '') + '>女生 <input type="radio" name="gender" value="1" ' + (userList.gender == 1 ? 'checked' : '') + '>男生</td>');
+
+            tr.append('<td><img src="data:image/png;base64,' + userList.avatar + '" alt="User Avatar" style="width:50px;height:50px;"></td>');
+
+            tr.append('<td><input type="radio" name="isDelete" value="0" ' + (userList.isDelete == 0 ? 'checked' : '') + '>否 <input type="radio" name="isDelete" value="1" ' + (userList.isDelete == 1 ? 'checked' : '') + '>是</td>');
+            // tr.append('<td>' + (userList.isDelete ? '是' : '否') + '</td>');
+            tr.append('<td>' +
+                '<button class="btn btn-success btn-sm userUpdata" onclick="userUpdataEventFinish(this)">確定</button>' +
+                '<button class="btn btn-warning btn-sm" onclick="getUserData()">取消</button>' +
+                '</td>');
+            tbody.append(tr);
+            // });
+            table.append(tbody);
+
+            memberContainer.append(table);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log('AJAX請求失敗：' + textStatus + ', ' + errorThrown);
+        }
+    });
+}
