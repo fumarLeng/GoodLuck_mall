1f6edebf18e63641a3f0c56e2fdfd9beb6d57df8
CuttleFish <b9804320975@gmail.com>
2024-01-26 17:39:05 +0800

    後台首頁 會員管理 前端 init demo3 productUpdataEventFinish Function 待做

--- src/main/resources/static/js/backStage/getProductData.js
+++ src/main/resources/static/js/backStage/getProductData.js
@@ -19,14 +19,13 @@ function getProductData() {
                 '<th class="member-list-th">商品賣點</th>' +
                 '<th class="member-list-th">商品單價</th>' +
                 '<th class="member-list-th">庫存數</th>' +
-                '<th class="member-list-th">圖片</th>' +
+                '<th class="member-list-th">圖片存放路徑</th>' +
                 '<th class="member-list-th">商品狀態</th>' +
                 '<th class="member-list-th">優先級</th>' +
                 '<th class="member-list-th">創建時間</th>' +
                 '<th class="member-list-th">最後修改時間</th>' +
-
-                // '<th class="member-list-th">創建人</th>' +
-                // '<th class="member-list-th">最後修改人</th>' +
+                '<th class="member-list-th">創建人</th>' +
+                '<th class="member-list-th">最後修改人</th>' +
                 '<th class="member-list-th">操作</th>' +
                 '</tr>');
             table.append(thead);
@@ -44,6 +43,8 @@ function getProductData() {
                 tr.append('<td>' + product.image + '</td>');
                 tr.append('<td>' + product.status + '</td>');
                 tr.append('<td>' + product.priority + '</td>');
+                tr.append('<td name="id" >' + product.createdTime + '</td>');
+                tr.append('<td name="id" >' + product.modifiedTime + '</td>');
                 tr.append('<td>' + product.createdTime + '</td>');
                 tr.append('<td>' + product.modifiedTime + '</td>');
 
@@ -78,7 +79,7 @@ function productUpdataEvent(clickedButton) {
         type: 'get',
         dataType: 'json',
         success: function (data) {
-            let userList = data;
+            let productList = data;
             let productContainer = $('#product');
             productContainer.empty();
 
@@ -93,34 +94,42 @@ function productUpdataEvent(clickedButton) {
                 '<th class="member-list-th">商品賣點</th>' +
                 '<th class="member-list-th">商品單價</th>' +
                 '<th class="member-list-th">庫存數</th>' +
-                '<th class="member-list-th">圖片</th>' +
+                '<th class="member-list-th">圖片存放路徑</th>' +
                 '<th class="member-list-th">商品狀態</th>' +
                 '<th class="member-list-th">優先級</th>' +
                 '<th class="member-list-th">創建時間</th>' +
                 '<th class="member-list-th">最後修改時間</th>' +
+                '<th class="member-list-th">創建人</th>' +
+                '<th class="member-list-th">最後修改人</th>' +
+                '<th class="member-list-th">操作</th>' +
                 '</tr>');
             table.append(thead);
 
             let tbody = $('<tbody></tbody>');
-            // $.each(userList, function (index, user) {
             let tr = $('<tr></tr>');
-            tr.append('<td name="uid" >' + userList.uid + '</td>');
-            tr.append('<td name="username" >' + userList.username + '</td>');
-            tr.append('<td><input class="user-input" name="phone" value="' + userList.phone + '"></td>');
-            tr.append('<td><input class="user-input" name="email" value="' + userList.email + '"></td>');
-            // tr.append('<td><input value="' + (userList.gender == 0 ? '女生' : '男生') + '"></td>');
-            tr.append('<td><input name="gender" type="radio" name="gender" value="0" ' + (userList.gender == 0 ? 'checked' : '') + '>女生 <input type="radio" name="gender" value="1" ' + (userList.gender == 1 ? 'checked' : '') + '>男生</td>');
+                tr.append('<td name="id" >' + productList.id + '</td>');
+                tr.append('<td name="id" >' + productList.categoryId + '</td>');
+                tr.append('<td name="id" >' + productList.itemType + '</td>');
+                tr.append('<td name="id" >' + productList.title + '</td>');
+                tr.append('<td name="id" >' + productList.sellPoint + '</td>');
+                tr.append('<td name="id" >' + productList.price + '</td>');
+                tr.append('<td name="id" >' + productList.num + '</td>');
+                tr.append('<td name="id" >' + productList.image + '</td>');
+                tr.append('<td name="id" >' + productList.status + '</td>');
+                tr.append('<td name="id" >' + productList.priority + '</td>');
+                tr.append('<td name="id" >' + productList.createdTime + '</td>');
+                tr.append('<td name="id" >' + productList.modifiedTime + '</td>');
+                tr.append('<td name="id" >' + productList.createdUser + '</td>');
+                tr.append('<td name="id" >' + productList.modifiedUser + '</td>');
 
-            tr.append('<td><img src="data:image/png;base64,' + userList.avatar + '" alt="User Avatar" style="width:50px;height:50px;"></td>');
-
-            tr.append('<td><input type="radio" name="isDelete" value="0" ' + (userList.isDelete == 0 ? 'checked' : '') + '>否 <input type="radio" name="isDelete" value="1" ' + (userList.isDelete == 1 ? 'checked' : '') + '>是</td>');
-            // tr.append('<td>' + (userList.isDelete ? '是' : '否') + '</td>');
+            // tr.append('<td><input type="radio" name="isDelete" value="0" ' + (userList.isDelete == 0 ? 'checked' : '') + '>否 <input type="radio" name="isDelete" value="1" ' + (userList.isDelete == 1 ? 'checked' : '') + '>是</td>');
+            // // tr.append('<td>' + (userList.isDelete ? '是' : '否') + '</td>');
             tr.append('<td>' +
-                '<button class="btn btn-success btn-sm userUpdata" onclick="userUpdataEventFinish(this)">確定</button>' +
-                '<button class="btn btn-warning btn-sm" onclick="getUserData()">取消</button>' +
+                '<button class="btn btn-success btn-sm userUpdata" onclick="productUpdataEventFinish(this)">確定</button>' +
+                '<button class="btn btn-warning btn-sm" onclick="getProductData()">取消</button>' +
                 '</td>');
             tbody.append(tr);
-            // });
+
             table.append(tbody);
 
             productContainer.append(table);
