9f9a6fe8801c1f7a9f079904a4f98c1d4d891c6c
CuttleFish <b9804320975@gmail.com>
2024-01-29 13:53:06 +0800

    後台首頁 User Controller 分類

---
+++ src/main/java/com/cy/store/controller/BackStageController/BSUserController.java
@@ -0,0 +1,70 @@
+package com.cy.store.controller.BackStageController;
+
+import com.cy.store.entity.User;
+import com.cy.store.service.impl.BackStageUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Controller
+@RequestMapping("BackStage")
+public class BSUserController {
+
+    //頁面資料
+
+    @Autowired
+    private BackStageUserService UserService;
+    @GetMapping("/user")
+    public ResponseEntity<List<User>> getAllUserData() {
+        List<User> userList = UserService.getAllUserData();
+        return ResponseEntity.ok(userList);
+    }
+
+    //單筆查詢user
+    @GetMapping("/user/{uid}")
+    public ResponseEntity<?> GetOneUser(@PathVariable Integer uid) {
+        List<User> userList = UserService.getAllUserData();
+        User updatedUser = null;
+        for (User user : userList) {
+            if (uid.equals(user.getUid())) {
+                updatedUser = user;
+                break;
+            }
+        }
+
+        if (updatedUser != null) {
+
+            return ResponseEntity.ok(updatedUser);
+        } else {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found with id: " + uid);
+        }
+    }
+
+    //修改
+    @PutMapping("/user/modify/{uid}")
+    public ResponseEntity<?> updateUser(@PathVariable Integer uid,
+                                        @RequestBody User updatedUser) {
+        // 根據uid查找用戶，然後更新用戶資訊
+        User existingUser = UserService.findUserById(uid);
+        System.out.println("existingUser: " + existingUser);
+        if (existingUser != null) {
+            // 更新用戶資訊
+            existingUser.setUsername(updatedUser.getUsername());
+            existingUser.setPhone(updatedUser.getPhone());
+            existingUser.setEmail(updatedUser.getEmail());
+            existingUser.setGender(updatedUser.getGender());
+            existingUser.setIsDelete(updatedUser.getIsDelete());
+
+            // 保存更新後的用戶資訊
+            UserService.saveUser(existingUser);
+
+            return ResponseEntity.ok(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("找不到該用戶", HttpStatus.NOT_FOUND);
+        }
+    }
+}
--- src/main/java/com/cy/store/controller/BackStageController/BackStageController.java
+++ src/main/java/com/cy/store/controller/BackStageController/BackStageController.java
@@ -1,4 +1,4 @@
-package com.cy.store.controller;
+package com.cy.store.controller.BackStageController;
 
 import com.cy.store.entity.Order;
 import com.cy.store.entity.Product;
@@ -28,64 +28,6 @@ public class BackStageController {
         return "forward:/web/BackStage/index.html";
     }
 
-    //頁面資料
-    @GetMapping("/user")
-    public ResponseEntity<List<User>> getAllUserData() {
-        List<User> userList = UserService.getAllUserData();
-//        System.out.println("Avatar: " + userList.get(1).getAvatar());
-        return ResponseEntity.ok(userList);
-    }
-
-    //單筆查詢user
-    @GetMapping("/user/{uid}")
-    public ResponseEntity<?> GetOneUser(@PathVariable Integer uid) {
-        List<User> userList = UserService.getAllUserData();
-        User updatedUser = null;
-        for (User user : userList) {
-            if (uid.equals(user.getUid())) {
-                updatedUser = user;
-                //找到
-//                System.out.println("ok");
-                break;
-            }
-        }
-
-        if (updatedUser != null) {
-
-            return ResponseEntity.ok(updatedUser);
-        } else {
-
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found with id: " + uid);
-        }
-    }
-
-    //修改
-    @PutMapping("/user/modify/{uid}")
-    public ResponseEntity<?> updateUser(@PathVariable Integer uid,
-                                        @RequestBody User updatedUser) {
-        // 根據uid查找用戶，然後更新用戶資訊
-        User existingUser = UserService.findUserById(uid);
-        System.out.println("existingUser: " + existingUser);
-        if (existingUser != null) {
-            // 更新用戶資訊
-            existingUser.setUsername(updatedUser.getUsername());
-            existingUser.setPhone(updatedUser.getPhone());
-            existingUser.setEmail(updatedUser.getEmail());
-            existingUser.setGender(updatedUser.getGender());
-            existingUser.setIsDelete(updatedUser.getIsDelete());
-
-//            System.out.println("uid: " + updatedUser.getUid());
-//            System.out.println("Gender: " + updatedUser.getPhone());
-//            System.out.println("IsDelete: " + updatedUser.getEmail());
-            // 保存更新後的用戶資訊
-            UserService.saveUser(existingUser);
-
-//            return new ResponseEntity<>("用戶更新成功", HttpStatus.OK);
-            return ResponseEntity.ok(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("找不到該用戶", HttpStatus.NOT_FOUND);
-        }
-    }
 
 
     @GetMapping("/order")
