39598302279ba1148cf3184c34f1fe2cb515cdba
leng <fumarkk23@gmail.com>
2024-02-07 22:37:32 +0800

    微調錯誤驗證

--- src/main/resources/static/js/frontend/cart/login/comSweetAlert2.js
+++ src/main/resources/static/js/frontend/cart/login/comSweetAlert2.js
@@ -16,44 +16,4 @@ const Toast = Swal.mixin({
         toast.addEventListener('mouseleave', Swal.resumeTimer)
     }
 });
-// 登入
-function setupLogin() {
-    $("#btn-login").click(function() {
-        $.ajax({
-            url: "/users/login",
-            type: "POST",
-            data: $("#form-login").serialize(),
-            dataType: "json",
-            success: function(json) {
-                if (json.state === 200) {
 
-                    Toast.fire({
-                        icon: 'success',
-                        title: '登入成功~',
-                    }).then(() => {
-                        setTimeout(() => {
-                            window.location.href = "index.html";
-                        }, 100);
-                    });
-
-                } else {
-
-                    Toast.fire({
-                        icon: 'error',
-                        title: '登入失敗'
-                    });
-                }
-            },
-            error: function(xhr, status, error) {
-
-                Toast.fire({
-                    icon: 'error',
-                    title: '請求失敗，私服器忙線中，請稍後~'
-                });
-            }
-        });
-    });
-}
-$(document).ready(function() {
-    setupLogin();
-});
